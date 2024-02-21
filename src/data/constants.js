ffe566e6f6fd33b4b41c280fef77d12609ec046b
Sai Prathik R <saiprathik2419@gmail.com>
2024-02-21 22:58:52 +0530

    Update resume link in Bio

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/prathik2401",
     resume:
-      "https://drive.google.com/file/d/1a7absQWkuIyryZDkONRHyIYR7pXD1-D_/view?usp=sharing",
+      "https://drive.google.com/file/d/1oh_NVDb9ENs3AfSvp6xACtGM2BBRmbq8/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/prathik2401/",
     twitter: "https://twitter.com/saiprathik2419",
     insta: "https://www.instagram.com/prathik.24/",
